92fd3df347ec66ed943bc71aae11ac20cfaa4d66
ariabee <abrown72@uvm.edu>
2020-05-23 12:16:28 -0400

    added TechJobsController to create and pass actionChoices to the model for every Controller that extends it

---
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/TechJobsController.java
@@ -0,0 +1,28 @@
+package org.launchcode.javawebdevtechjobsmvc.controllers;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.HashMap;
+
+public class TechJobsController {
+
+    private static HashMap<String, String> actionChoices = new HashMap<>();
+
+    public TechJobsController() {
+        actionChoices.put("search", "Search");
+        actionChoices.put("list", "List");
+    }
+
+    /**
+     * @ ModelAttribute("actions") annotation will cause the return value of the method
+     * to be set in the model with key "actions" for every controller that extends TechJobsController.
+     *
+     * (i.e. instead of saying model.addAttribute("actions", actionChoices) inside every controller method)
+     **/
+    @ModelAttribute("actions")
+    public static HashMap<String, String> getActionChoices() {
+        return actionChoices;
+    }
+
+    // Finally, update your code so that columnChoices also lives in TechJobsController.
+}
